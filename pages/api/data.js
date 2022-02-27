dff09095f9d02bae167335f004195fbfa8e04b7f
Frezeh <ezehfrank87@gmail.com>
2022-02-27 10:27:05 +0100

    initial commit

---
+++ pages/api/data.js
@@ -0,0 +1,213 @@
+export const data = [
+  {
+    id: 1,
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin vitae dui condimentum, lacinia velit sit amet, pulvinar metus. Vivamus vitae vehicula leo. Suspendisse tempor, felis aliquam accumsan dapibus, quam justo tristique libero, vitae laoreet ligula arcu sollicitudin ipsum. Maecenas ac nibh ultrices, faucibus ante blandit, ullamcorper nibh. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Fusce nec tempor nibh. Nulla sapien magna, rhoncus in felis ac, dignissim feugiat justo. Quisque pretium placerat enim, eget laoreet quam pharetra vel.",
+    name: "Acetaminophen",
+    frequency: "3 times/day",
+    duration: "1/Month",
+    dosage: "400mg",
+    featured: "painkiller"
+  },
+  {
+    id: 2,
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin vitae dui condimentum, lacinia velit sit amet, pulvinar metus. Vivamus vitae vehicula leo. Suspendisse tempor, felis aliquam accumsan dapibus, quam justo tristique libero, vitae laoreet ligula arcu sollicitudin ipsum. Maecenas ac nibh ultrices, faucibus ante blandit, ullamcorper nibh. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Fusce nec tempor nibh. Nulla sapien magna, rhoncus in felis ac, dignissim feugiat justo. Quisque pretium placerat enim, eget laoreet quam pharetra vel.",
+    name: "Brilinta",
+    frequency: "3 times/day",
+    duration: "1/Month",
+    dosage: "200mg",
+    featured: "painkiller"
+  },
+  {
+    id: 3,
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin vitae dui condimentum, lacinia velit sit amet, pulvinar metus. Vivamus vitae vehicula leo. Suspendisse tempor, felis aliquam accumsan dapibus, quam justo tristique libero, vitae laoreet ligula arcu sollicitudin ipsum. Maecenas ac nibh ultrices, faucibus ante blandit, ullamcorper nibh. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Fusce nec tempor nibh. Nulla sapien magna, rhoncus in felis ac, dignissim feugiat justo. Quisque pretium placerat enim, eget laoreet quam pharetra vel.",
+    name: "Cephalexin",
+    frequency: "3 times/day",
+    duration: "1/Month",
+    dosage: "100mg",
+    featured: "painkiller"
+  },
+  {
+    id: 4,
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin vitae dui condimentum, lacinia velit sit amet, pulvinar metus. Vivamus vitae vehicula leo. Suspendisse tempor, felis aliquam accumsan dapibus, quam justo tristique libero, vitae laoreet ligula arcu sollicitudin ipsum. Maecenas ac nibh ultrices, faucibus ante blandit, ullamcorper nibh. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Fusce nec tempor nibh. Nulla sapien magna, rhoncus in felis ac, dignissim feugiat justo. Quisque pretium placerat enim, eget laoreet quam pharetra vel.",
+    name: "Doxycycline",
+    frequency: "3 times/day",
+    duration: "1/Month",
+    dosage: "300mg",
+    featured: "painkiller"
+  },
+  {
+    id: 5,
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin vitae dui condimentum, lacinia velit sit amet, pulvinar metus. Vivamus vitae vehicula leo. Suspendisse tempor, felis aliquam accumsan dapibus, quam justo tristique libero, vitae laoreet ligula arcu sollicitudin ipsum. Maecenas ac nibh ultrices, faucibus ante blandit, ullamcorper nibh. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Fusce nec tempor nibh. Nulla sapien magna, rhoncus in felis ac, dignissim feugiat justo. Quisque pretium placerat enim, eget laoreet quam pharetra vel.",
+    name: "Entresto",
+    frequency: "3 times/day",
+    duration: "1/Month",
+    dosage: "400mg",
+    featured: "painkiller"
+  },
+  {
+    id: 6,
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin vitae dui condimentum, lacinia velit sit amet, pulvinar metus. Vivamus vitae vehicula leo. Suspendisse tempor, felis aliquam accumsan dapibus, quam justo tristique libero, vitae laoreet ligula arcu sollicitudin ipsum. Maecenas ac nibh ultrices, faucibus ante blandit, ullamcorper nibh. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Fusce nec tempor nibh. Nulla sapien magna, rhoncus in felis ac, dignissim feugiat justo. Quisque pretium placerat enim, eget laoreet quam pharetra vel.",
+    name: "Farxiga",
+    frequency: "3 times/day",
+    duration: "1/Month",
+    dosage: "400mg",
+    featured: "painkiller"
+  },  
+  {
+    id: 7,
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin vitae dui condimentum, lacinia velit sit amet, pulvinar metus. Vivamus vitae vehicula leo. Suspendisse tempor, felis aliquam accumsan dapibus, quam justo tristique libero, vitae laoreet ligula arcu sollicitudin ipsum. Maecenas ac nibh ultrices, faucibus ante blandit, ullamcorper nibh. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Fusce nec tempor nibh. Nulla sapien magna, rhoncus in felis ac, dignissim feugiat justo. Quisque pretium placerat enim, eget laoreet quam pharetra vel.",
+    name: "Gabapentin",
+    frequency: "3 times/day",
+    duration: "1/Month",
+    dosage: "400mg",
+    featured: "painkiller"
+  },
+  {
+    id: 8,
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin vitae dui condimentum, lacinia velit sit amet, pulvinar metus. Vivamus vitae vehicula leo. Suspendisse tempor, felis aliquam accumsan dapibus, quam justo tristique libero, vitae laoreet ligula arcu sollicitudin ipsum. Maecenas ac nibh ultrices, faucibus ante blandit, ullamcorper nibh. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Fusce nec tempor nibh. Nulla sapien magna, rhoncus in felis ac, dignissim feugiat justo. Quisque pretium placerat enim, eget laoreet quam pharetra vel.",
+    name: "Humira",
+    frequency: "3 times/day",
+    duration: "1/Month",
+    dosage: "400mg",
+    featured: "malaria"
+  },
+  {
+    id: 9,
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin vitae dui condimentum, lacinia velit sit amet, pulvinar metus. Vivamus vitae vehicula leo. Suspendisse tempor, felis aliquam accumsan dapibus, quam justo tristique libero, vitae laoreet ligula arcu sollicitudin ipsum. Maecenas ac nibh ultrices, faucibus ante blandit, ullamcorper nibh. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Fusce nec tempor nibh. Nulla sapien magna, rhoncus in felis ac, dignissim feugiat justo. Quisque pretium placerat enim, eget laoreet quam pharetra vel.",
+    name: "Hydroxychloroquine",
+    frequency: "3 times/day",
+    duration: "1/Month",
+    dosage: "300mg",
+    featured: "malaria"
+  },
+  {
+    id: 10,
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin vitae dui condimentum, lacinia velit sit amet, pulvinar metus. Vivamus vitae vehicula leo. Suspendisse tempor, felis aliquam accumsan dapibus, quam justo tristique libero, vitae laoreet ligula arcu sollicitudin ipsum. Maecenas ac nibh ultrices, faucibus ante blandit, ullamcorper nibh. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Fusce nec tempor nibh. Nulla sapien magna, rhoncus in felis ac, dignissim feugiat justo. Quisque pretium placerat enim, eget laoreet quam pharetra vel.",
+    name: "Invokana",
+    frequency: "3 times/day",
+    duration: "1/Month",
+    dosage: "400mg",
+    featured: "malaria"
+  },
+  {
+    id: 11,
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin vitae dui condimentum, lacinia velit sit amet, pulvinar metus. Vivamus vitae vehicula leo. Suspendisse tempor, felis aliquam accumsan dapibus, quam justo tristique libero, vitae laoreet ligula arcu sollicitudin ipsum. Maecenas ac nibh ultrices, faucibus ante blandit, ullamcorper nibh. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Fusce nec tempor nibh. Nulla sapien magna, rhoncus in felis ac, dignissim feugiat justo. Quisque pretium placerat enim, eget laoreet quam pharetra vel.",
+    name: "Jardiance",
+    frequency: "3 times/day",
+    duration: "1/Month",
+    dosage: "400mg",
+    featured: "malaria"
+  },
+  {
+    id: 12,
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin vitae dui condimentum, lacinia velit sit amet, pulvinar metus. Vivamus vitae vehicula leo. Suspendisse tempor, felis aliquam accumsan dapibus, quam justo tristique libero, vitae laoreet ligula arcu sollicitudin ipsum. Maecenas ac nibh ultrices, faucibus ante blandit, ullamcorper nibh. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Fusce nec tempor nibh. Nulla sapien magna, rhoncus in felis ac, dignissim feugiat justo. Quisque pretium placerat enim, eget laoreet quam pharetra vel.",
+    name: "Kevzara",
+    frequency: "3 times/day",
+    duration: "1/Month",
+    dosage: "300mg",
+    featured: "malaria"
+  },  
+  {
+    id: 13,
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin vitae dui condimentum, lacinia velit sit amet, pulvinar metus. Vivamus vitae vehicula leo. Suspendisse tempor, felis aliquam accumsan dapibus, quam justo tristique libero, vitae laoreet ligula arcu sollicitudin ipsum. Maecenas ac nibh ultrices, faucibus ante blandit, ullamcorper nibh. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Fusce nec tempor nibh. Nulla sapien magna, rhoncus in felis ac, dignissim feugiat justo. Quisque pretium placerat enim, eget laoreet quam pharetra vel.",
+    name: "Lofexidine",
+    frequency: "3 times/day",
+    duration: "1/Month",
+    dosage: "400mg",
+    featured: "malaria"
+  },
+  {
+    id: 14,
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin vitae dui condimentum, lacinia velit sit amet, pulvinar metus. Vivamus vitae vehicula leo. Suspendisse tempor, felis aliquam accumsan dapibus, quam justo tristique libero, vitae laoreet ligula arcu sollicitudin ipsum. Maecenas ac nibh ultrices, faucibus ante blandit, ullamcorper nibh. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Fusce nec tempor nibh. Nulla sapien magna, rhoncus in felis ac, dignissim feugiat justo. Quisque pretium placerat enim, eget laoreet quam pharetra vel.",
+    name: "Meloxicam",
+    frequency: "3 times/day",
+    duration: "1/Month",
+    dosage: "400mg",
+    featured: "malaria"
+  },
+  {
+    id: 15,
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin vitae dui condimentum, lacinia velit sit amet, pulvinar metus. Vivamus vitae vehicula leo. Suspendisse tempor, felis aliquam accumsan dapibus, quam justo tristique libero, vitae laoreet ligula arcu sollicitudin ipsum. Maecenas ac nibh ultrices, faucibus ante blandit, ullamcorper nibh. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Fusce nec tempor nibh. Nulla sapien magna, rhoncus in felis ac, dignissim feugiat justo. Quisque pretium placerat enim, eget laoreet quam pharetra vel.",
+    name: "Omeprazole",
+    frequency: "3 times/day",
+    duration: "1/Month",
+    dosage: "400mg",
+    featured: "malaria"
+  },
+  {
+    id: 16,
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin vitae dui condimentum, lacinia velit sit amet, pulvinar metus. Vivamus vitae vehicula leo. Suspendisse tempor, felis aliquam accumsan dapibus, quam justo tristique libero, vitae laoreet ligula arcu sollicitudin ipsum. Maecenas ac nibh ultrices, faucibus ante blandit, ullamcorper nibh. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Fusce nec tempor nibh. Nulla sapien magna, rhoncus in felis ac, dignissim feugiat justo. Quisque pretium placerat enim, eget laoreet quam pharetra vel.",
+    name: "Probuphine",
+    frequency: "3 times/day",
+    duration: "1/Month",
+    dosage: "400mg",
+    featured: "vitamins"
+  },
+  {
+    id: 17,
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin vitae dui condimentum, lacinia velit sit amet, pulvinar metus. Vivamus vitae vehicula leo. Suspendisse tempor, felis aliquam accumsan dapibus, quam justo tristique libero, vitae laoreet ligula arcu sollicitudin ipsum. Maecenas ac nibh ultrices, faucibus ante blandit, ullamcorper nibh. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Fusce nec tempor nibh. Nulla sapien magna, rhoncus in felis ac, dignissim feugiat justo. Quisque pretium placerat enim, eget laoreet quam pharetra vel.",
+    name: "Rybelsus",
+    frequency: "3 times/day",
+    duration: "1/Month",
+    dosage: "400mg",
+    featured: "vitamins"
+  },
+  {
+    id: 18,
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin vitae dui condimentum, lacinia velit sit amet, pulvinar metus. Vivamus vitae vehicula leo. Suspendisse tempor, felis aliquam accumsan dapibus, quam justo tristique libero, vitae laoreet ligula arcu sollicitudin ipsum. Maecenas ac nibh ultrices, faucibus ante blandit, ullamcorper nibh. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Fusce nec tempor nibh. Nulla sapien magna, rhoncus in felis ac, dignissim feugiat justo. Quisque pretium placerat enim, eget laoreet quam pharetra vel.",
+    name: "secukinumab",
+    frequency: "3 times/day",
+    duration: "1/Month",
+    dosage: "400mg",
+    featured: "vitamins"
+  },
+  {
+    id: 19,
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin vitae dui condimentum, lacinia velit sit amet, pulvinar metus. Vivamus vitae vehicula leo. Suspendisse tempor, felis aliquam accumsan dapibus, quam justo tristique libero, vitae laoreet ligula arcu sollicitudin ipsum. Maecenas ac nibh ultrices, faucibus ante blandit, ullamcorper nibh. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Fusce nec tempor nibh. Nulla sapien magna, rhoncus in felis ac, dignissim feugiat justo. Quisque pretium placerat enim, eget laoreet quam pharetra vel.",
+    name: "Tramadol",
+    frequency: "3 times/day",
+    duration: "1/Month",
+    dosage: "400mg",
+    featured: "vitamins"
+  },
+  {
+    id: 20,
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin vitae dui condimentum, lacinia velit sit amet, pulvinar metus. Vivamus vitae vehicula leo. Suspendisse tempor, felis aliquam accumsan dapibus, quam justo tristique libero, vitae laoreet ligula arcu sollicitudin ipsum. Maecenas ac nibh ultrices, faucibus ante blandit, ullamcorper nibh. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Fusce nec tempor nibh. Nulla sapien magna, rhoncus in felis ac, dignissim feugiat justo. Quisque pretium placerat enim, eget laoreet quam pharetra vel.",
+    name: "Viagra",
+    frequency: "3 times/day",
+    duration: "1/Month",
+    dosage: "100mg",
+    featured: "vitamins"
+  },
+  {
+    id: 21,
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin vitae dui condimentum, lacinia velit sit amet, pulvinar metus. Vivamus vitae vehicula leo. Suspendisse tempor, felis aliquam accumsan dapibus, quam justo tristique libero, vitae laoreet ligula arcu sollicitudin ipsum. Maecenas ac nibh ultrices, faucibus ante blandit, ullamcorper nibh. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Fusce nec tempor nibh. Nulla sapien magna, rhoncus in felis ac, dignissim feugiat justo. Quisque pretium placerat enim, eget laoreet quam pharetra vel.",
+    name: "Wellbutrin",
+    frequency: "3 times/day",
+    duration: "1/Month",
+    dosage: "200mg",
+    featured: "vitamins"
+  },
+  {
+    id: 22,
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin vitae dui condimentum, lacinia velit sit amet, pulvinar metus. Vivamus vitae vehicula leo. Suspendisse tempor, felis aliquam accumsan dapibus, quam justo tristique libero, vitae laoreet ligula arcu sollicitudin ipsum. Maecenas ac nibh ultrices, faucibus ante blandit, ullamcorper nibh. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Fusce nec tempor nibh. Nulla sapien magna, rhoncus in felis ac, dignissim feugiat justo. Quisque pretium placerat enim, eget laoreet quam pharetra vel.",
+    name: "Xanax",
+    frequency: "3 times/day",
+    duration: "1/Month",
+    dosage: "400mg",
+    featured: "vitamins"
+  },
+  {
+    id: 23,
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin vitae dui condimentum, lacinia velit sit amet, pulvinar metus. Vivamus vitae vehicula leo. Suspendisse tempor, felis aliquam accumsan dapibus, quam justo tristique libero, vitae laoreet ligula arcu sollicitudin ipsum. Maecenas ac nibh ultrices, faucibus ante blandit, ullamcorper nibh. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Fusce nec tempor nibh. Nulla sapien magna, rhoncus in felis ac, dignissim feugiat justo. Quisque pretium placerat enim, eget laoreet quam pharetra vel.", 
+    name: "Zubsolv",
+    frequency: "3 times/day",
+    duration: "1/Month",
+    dosage: "200mg",
+    featured: "vitamins"
+  }
+]
+
+export default function handler(req, res) {
+  res.status(200).json(data);
+}
---
+++ pages/details.js
@@ -0,0 +1,11 @@
+import { useRouter } from 'next/router'
+
+function details() {
+  return (
+    <div>
+      <h1>Post Slug: EEEEE</h1>
+    </div>
+  )
+}
+
+export default details
